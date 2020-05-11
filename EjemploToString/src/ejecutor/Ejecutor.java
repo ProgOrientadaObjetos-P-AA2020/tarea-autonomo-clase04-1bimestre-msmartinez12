18fb1055265c940697a3f85f14500dea9a87f291
Mateo Martinez <msmartinez12@utpl.edu.ec>
2020-05-11 01:05:36 -0500

    ejemplo practico del toString

---
+++ EjemploToString/src/ejecutor/Ejecutor.java
@@ -0,0 +1,15 @@
+
+package ejecutor;
+
+/**
+ * @author mateo
+ */
+public class Ejecutor {
+
+    public static void main(String[] args) {
+         Nombre nombre1 = new Nombre("Mateo");
+	    
+		System.out.print(nombre1);
+    }
+    
+}
---
+++ EjemploToString/src/ejecutor/Nombre.java
@@ -0,0 +1,18 @@
+
+package ejecutor;
+
+/**
+* @author mateo
+*/
+public class Nombre {
+    private String nombre;
+	
+	public Nombre (String n){
+		nombre = n;
+	}
+
+	public String toString(){
+		return "Hola mundo mi nombre es:  " + nombre; 
+	}
+    
+}
